dd2d658718b16e1f94a4e9f9d85d5196e91aea5d
Tom Robinson <tjrobinson@users.noreply.github.com>
2024-02-10 18:16:30 +0000

    Move new content into Obsidian folder

--- content/bliki/tools/index.md
+++
@@ -1,5 +0,0 @@
----
-title: Tools
----
-
-Work in progress...
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,38 +11,14 @@ class SiteIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <a rel="me" href="https://hachyderm.io/@tjrobinson">Mastodon</a>
-        <h2>Bliki pages</h2>
-        <p>A cross between a blog and a wiki - content which is not specific to a particular point in time, and is being updated.</p>
-        {posts
-          .filter(({ node }) => node.fileAbsolutePath.includes("bliki"))
-          .map(({ node }) => {
-            const title = node.frontmatter.title || node.fields.slug
-            return (
-              <div class="max-w-full my-2" key={node.fields.slug}>
-                  <div class="mb-2"><Link to={node.fields.slug}>{title}</Link></div>
-                  <p class="text-grey-darker text-base">
-                  {node.frontmatter.slug}
-                  </p>
-              </div>
-            )
-          })}
-
-        <h2>Blog posts</h2>
-        <p>Older posts may no longer be accurate or up to date!</p>
-          {posts
-            .filter(({ node }) => node.fileAbsolutePath.includes("blog"))
-            .map(({ node }) => {
-              const title = node.frontmatter.title || node.fields.slug
-              return (
-                <div class="max-w-full my-2" key={node.fields.slug}>
-                    <div class="mb-0"><Link to={node.fields.slug}>{title}</Link></div>
-                    <div class="text-grey-darker text-base">
-                    {node.frontmatter.date}
-                    </div>
-                </div>
-              )
-            })}
+        <ul>
+          <li>
+            <a rel="me" href="https://hachyderm.io/@tjrobinson">Mastodon</a>
+          </li>
+          <li>
+            <a rel="me" href="https://publish.obsidian.md/tjrobinson">Digital Garden</a>
+          </li>
+        </ul>
       </Layout>
     )
   }
