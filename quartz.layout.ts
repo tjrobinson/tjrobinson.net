8e7e33da43ca6f1c1dea8ccb6e8d59386ac2253c
Tom Robinson <tom@tjrobinson.net>
2026-03-08 16:59:19 +0000

    chore: remove default footer links from layout configuration.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,10 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   afterBody: [],
   footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
+    links: {},
   }),
 };
 
